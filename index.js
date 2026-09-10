695c023884dd0fff5ac925e1dc38c9d7a84d1807
Hgregori <Hgregorio@byupathway.edu>
2026-09-09 21:02:09 -0300

    W02-TA

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
@@ -10,10 +14,7 @@ app
 
 const db = require('./models');
 db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(db.url)
   .then(() => {
     console.log('Connected to the database!');
   })
